07b4331ef20d65921ea9d2f21cb16d36682ef0d0
Kandukuru Udaya Sankar <kandukuruuday06@gmail.com>
2023-09-01 18:03:13 +0530

    Add EC2 Instance

---
+++ terraform/ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-0533f2ba8a1995cf9"
+  instance_type = "t2.micro"
+  key_name      = "MyKeyPairEC2"
+
+  subnet_id                   = aws_subnet.vpc_public_subnet_u.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    "Name" : "ec2 instance U1"
+  }
+}
